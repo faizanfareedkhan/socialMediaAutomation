8b17e4b9435576af6a61fa1cd1bbb9dd9f28499b
Akhtar Abbas <akhtarabbas909@hotmail.com>
2025-05-06 20:07:42 +0500

    updating the type of action method
    updating the type of action method

--- backend/routes/tempUserRoutes.js
+++ backend/routes/tempUserRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const tempUserController = require('../controllers/tempUserController');
 
 router.post('/signup', tempUserController.signup);
-router.get('/getUser/token', tempUserController.getUser);
+router.post('/getUser/token', tempUserController.getUser);
 
 
 module.exports = router;
