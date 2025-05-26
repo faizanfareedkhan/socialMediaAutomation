b624230c8c443da9d96c3c006c32d789c2642092
Akhtar Abbas <akhtarabbas909@hotmail.com>
2025-05-27 01:52:50 +0500

    posting video code implemented
    posting video code implemented

--- backend/services/facebookService.js
+++ backend/services/facebookService.js
@@ -4,8 +4,10 @@ const FormData = require('form-data');
 const axios = require('axios');
 
 
-const PAGE_ACCESS_TOKEN = 'your-page-access-token';
-const PAGE_ID = 'your-page-id';
+const PAGE_ACCESS_TOKEN = '1242413847612277';
+const APP_SECRET = 'a4ccaaca01e568f8c4e02aaa9d86588a';
+const PAGE_ID = '61576755361238';
+const CLIENT_TOKEN = 'd3fbeb3c5f330a20d2b4cb3df9d2ea4f';
 
 const postTextOnFacebook = async function(message) {
   try {
@@ -84,4 +86,28 @@ const postImagesByHeaderOnFacebook = async function(path) {
   }
 }
 
-module.exports={postTextOnFacebook, postImagesByURLOnFacebook, postImagesByHeaderOnFacebook};
+const postVideoByHeaderOnFacebook = async function(path) {
+  try {
+    const form = new FormData();
+    form.append('access_token', PAGE_ACCESS_TOKEN);
+    form.append('description', 'My video caption');
+    form.append('source', fs.createReadStream(path));
+
+    const response = await axios.post(
+      `https://graph.facebook.com/v19.0/${PAGE_ID}/videos`,
+      form,
+      { headers: form.getHeaders() }
+    );
+
+    res.json({
+      success: true,
+      videoId: response.data.id
+    });
+
+  } catch (error) {
+    console.error('Video Upload Error:', error.response?.data || error.message);
+    res.status(500).json({ error: 'Failed to upload video' });
+  }
+}
+
+module.exports={postTextOnFacebook, postImagesByURLOnFacebook, postImagesByHeaderOnFacebook, postVideoByHeaderOnFacebook};
